f3dad0d3f3ae28383ee54db1a5ae7444aecd2b66
Zach <geremiah_marasigan@dlsu.edu.ph>
2018-08-09 00:30:06 +0800

    Working user service?

--- 4UN/model/userService.js
+++ 4UN/model/userService.js
@@ -2,27 +2,36 @@ const mongoose = require("mongoose");
 var User = require("mongoose").model("User")
 var UserSchema = User.schema
 
+//Going to use Mongoose Queries, to be called in server by exec() function
 function getAll(){
-    var Users = User.find().then((users)=>{
-        console.log("User get success")
-        return users
-    },(err)=>{
-        return false  
-    });
+    var query = User.find()
+    
+    return query
 }
 
 //Find specific user given his username, to be used when you click a users name from a meme since meme only stores his username
-function findSpecific(query){
-    User.findOne({"username": query}).then((user)=>{
+//Also used in authentication
+function findSpecific(user){
+    var query = User.findOne({"username": user})/*.then((user)=>{
         console.log("User " + query + " Found")
         return user
     }, (err)=>{
         console.log("User not found")
         return false
-    })
+    })*/
+    return query
 }
 
 //Get the memes uploaded by a specific user, for profile page
+//only shows latest 12 memes
 function getMemes(user){
-    return user.posts
+    var query = User.findOne({username : user}, {posts: 1}).sort("_id":-1).limit(12)
+    return query
+}
+
+//Get the memes shared to a specific user, for profile page
+//only shows latest 12 memes
+function sharedMemes(user){
+    var query = User.findOne({username : user}, {shared: 1}).sort("_id":-1).limit(12)
+    return query
 }
