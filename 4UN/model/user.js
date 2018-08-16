1a85bdf04d22d24255d61e3d6ee183cbdf2cc68b
Zach <geremiah_marasigan@dlsu.edu.ph>
2018-08-16 09:15:06 +0800

    Started on Fixing

--- 4UN/model/user.js
+++ 4UN/model/user.js
@@ -1,7 +1,10 @@
 // create mongoose document ticket
 const mongoose = require("mongoose");
+const Meme = require("meme.js");
 var Meme = require("mongoose").model("Meme")
 var MemeSchema = Meme.schema
+
+var exports = module.exports = {};
 
 var UserSchema = mongoose.Schema({
     username: {
@@ -21,5 +24,75 @@ var UserSchema = mongoose.Schema({
 });
 
 var User = mongoose.model("User", UserSchema);
+
+//Going to use Mongoose Queries, to be called in server by exec() function
+exports.getAll = function(){
+    return new Promise(function(res, rej){
+        User.find().then((user)=>{
+            res(user)
+        }, (err)=>{
+            rej(err)
+        })
+    })
+}
+
+//Find specific user given his username, to be used when you click a users name from a meme since meme only stores his username
+//Also used in authentication
+exports.findSpecific = function (user){
+    return new Promise(function(res, rej){
+        User.findOne({"username": user}).then((user)=>{
+            console.log("User " + query + " Found")
+            res(user)
+        }, (err)=>{
+            console.log("User not found")
+            rej(err)
+        })
+        
+    }) 
+}
+
+exports.getHashedPasword = function(user){
+    var query = User.findOne({"username": user}, {password: 1})
+    return query
+}
+
+//Get the memes uploaded by a specific user, for profile page
+//only shows latest 12 memes
+exports.getMemes = function(user){
+    var query = User.findOne({username : user}, {posts: 1}).sort({_id:-1}).limit(12)
+    return query
+}
+
+//Get the memes shared to a specific user, for profile page
+//only shows latest 12 memes
+exports.sharedMemes = function(user){
+    var query = User.findOne({username : user}, {shared: 1}).sort({_id:-1}).limit(12)
+    return query
+}
+
+exports.removedShared = function(user, meme){
+    var query = User.findOne({username : user}).update({}, {$pull: {shared: meme}}, {multi: true})
+    return query
+}
+
+exports.updateShared = function(user, meme){
+    var query = User.findOne({username : user}).update({}, {$addToSet: {shared: meme}}, {multi: true})
+    return query
+}
+
+exports.removePost = function(user, meme){
+    var query = User.findOne({username : user}).update({}, {$pull: {posts: meme}}, {multi: true})
+    return query
+}
+
+exports.addPost = function(user, meme){
+    var query = User.findOne({username : user}).update({}, {$addToSet: {posts: meme}}, {multi: true})
+    return query
+}
+
+exports.addNewUser = function(user){
+    var query = user.save()
+    return query
+}
 
 module.exports = User
