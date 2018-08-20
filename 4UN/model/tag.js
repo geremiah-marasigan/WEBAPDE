5664c1cd1c2a8c36f1cc714ee6506beaf860cf43
Zach <geremiah_marasigan@dlsu.edu.ph>
2018-08-20 10:19:00 +0800

    Revert "Merge branch 'master' into old-state"
    This reverts commit 8197ab80c230b63deea81cdd8cac1ad5bbb7af24, reversing
    changes made to 7e16174c35d114f8b12225791202d491a66e7aa7.

--- 4UN/model/tag.js
+++ 4UN/model/tag.js
@@ -1,123 +1,17 @@
 // create mongoose document ticket
 const mongoose = require("mongoose");
-const Meme = require("meme.js");
+var Meme = require("mongoose").model("Meme")
 var MemeSchema = Meme.schema
 
 var TagSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 3,
-        trim: true
+    name: {type : String,
+        required : true,
+        minlength : 3,
+        trim : true
     },
     memes: [MemeSchema]
 });
 
 var Tag = mongoose.model("Tag", TagSchema);
 
-var exports = module.exports = {};
-
-exports.schema = TagSchema
-
-//Because our tags are made by us (no custom tags)
-exports.getAllNames = function () {
-    return new Promise(function (res, rej) {
-        Tag.find({}, {
-            name: 1
-        }).then((succ) => {
-            console.log("Get All successful" + memeid)
-            res(succ)
-        }, (err) => {
-            console.log("Get All Failed")
-            rej(err)
-        })
-
-    })
-
-}
-
-
-exports.getTag = function (tag) {
-    
-    return new Promise(function (res, rej) {
-        Tag.findOne({
-            "name": tag
-        }).then((succ) => {
-            console.log("Get All successful" + memeid)
-            res(succ)
-        }, (err) => {
-            console.log("Get All Failed")
-            rej(err)
-        })
-
-    })
-    
-}
-
-//Get the memes of a specific tag name
-//First 12
-exports.getMemes = function (tag) {
-    return new Promise(function (res, rej) {
-        Tag.findOne({
-            "name": tag
-            }, {
-            memes: 1
-        }).then((succ) => {
-            console.log("Get All successful" + memeid)
-            res(succ)
-        }, (err) => {
-            console.log("Get All Failed")
-            rej(err)
-        })
-    })
-}
-
-//Adds a meme to the specified tag
-exports.addMeme = function (name, meme) {
-    return new Promise(function (res, rej) {
-        Tag.findOne({
-            name: name
-        }).update({}, {
-            $addToSet: {
-                memes: meme
-            }
-        }, {
-            multi: true
-    }).then((succ) => {
-            console.log("Get All successful" + memeid)
-            res(succ)
-        }, (err) => {
-            console.log("Get All Failed")
-            rej(err)
-        })
-    })
-}
-
-exports.removeMeme = function (name, meme) {
-    return new Promise(function(res, rej){
-        Tag.findOne({
-            name: name
-        }).update({}, {
-            $pull: {
-                memes: meme
-            }
-        }, {
-            multi: true
-        })
-        
-    })
-    
-    
-}
-
-//just in case
-exports.addTag = function (tag) {
-    return new Promise(function(res, rej){
-    var t = new Tag(tag)
-    t.save().then((newUser)=>{
-      res(newUser)
-    }, (err)=>{
-      rej(err)
-    })
-  })
-}
+module.exports = Tag
--- 4UN/model/userService.js
+++ 4UN/model/userService.js
@@ -6,4 +6,68 @@ var exports = module.exports = {};
 var User = require("mongoose").model("User")
 var UserSchema = User.schema
 
+//Going to use Mongoose Queries, to be called in server by exec() function
+exports.getAll = function(){
+    var query = User.find()
+    
+    return query
+}
+
+//Find specific user given his username, to be used when you click a users name from a meme since meme only stores his username
+//Also used in authentication
+exports.findSpecific = function (user){
+    var query = User.findOne({"username": user})/*.then((user)=>{
+        console.log("User " + query + " Found")
+        return user
+    }, (err)=>{
+        console.log("User not found")
+        return false
+    })*/
+    return query
+}
+
+exports.getHashedPasword = function(user){
+    var query = User.findOne({"username": user}, {password: 1})
+    return query
+}
+
+//Get the memes uploaded by a specific user, for profile page
+//only shows latest 12 memes
+exports.getMemes = function(user){
+    var query = User.findOne({username : user}, {posts: 1}).sort({_id:-1}).limit(12)
+    return query
+}
+
+//Get the memes shared to a specific user, for profile page
+//only shows latest 12 memes
+exports.sharedMemes = function(user){
+    var query = User.findOne({username : user}, {shared: 1}).sort({_id:-1}).limit(12)
+    return query
+}
+
+exports.removedShared = function(user, meme){
+    var query = User.findOne({username : user}).update({}, {$pull: {shared: meme}}, {multi: true})
+    return query
+}
+
+exports.updateShared = function(user, meme){
+    var query = User.findOne({username : user}).update({}, {$addToSet: {shared: meme}}, {multi: true})
+    return query
+}
+
+exports.removePost = function(user, meme){
+    var query = User.findOne({username : user}).update({}, {$pull: {posts: meme}}, {multi: true})
+    return query
+}
+
+exports.addPost = function(user, meme){
+    var query = User.findOne({username : user}).update({}, {$addToSet: {posts: meme}}, {multi: true})
+    return query
+}
+
+exports.addNewUser = function(user){
+    var query = user.save()
+    return query
+}
+
 //add User in 
