00723d461ecc1e57dc8315e6e661eb70ef7153d2
Zach <geremiah_marasigan@dlsu.edu.ph>
2018-08-27 00:49:21 +0800

    fixed user

--- 4UN/model/user.js
+++ 4UN/model/user.js
@@ -57,7 +57,7 @@ exports.login = function (user){
 exports.findSpecific = function (user){
     return new Promise(function(res, rej){
         User.findOne(
-            {username: user.username
+            {username: user
             }
         ).then((founduser)=>{
             res(founduser)
